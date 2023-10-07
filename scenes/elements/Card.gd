b5cb01a56d430f898b492e80cb9af3d568db0ebd
rriver <riverocornejor@gmail.com>
2023-10-07 14:51:22 -0400

    removing trash

--- scenes/elements/Card.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var animation_player : AnimationPlayer
-
-func _ready():
-	animation_player = $AnimationPlayer
-
-func _on_card_clicked():
-	animation_player.play("flip") 
--- scenes/screens/Table.gd
+++ scenes/screens/Table.gd
@@ -1,22 +1,5 @@
 extends Node2D
 
-var card_block : Control
-const card_scene = preload("res://scenes/elements/Card.tscn")
-var arg = null
-
-func _ready():
-	print(get_viewport().size)
-	card_block = $Hand
-	for i in range(5):
-		var card_instance = card_scene.instantiate()
-		card_block.add_child(card_instance)
-
-func _process(delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction > 0:
-		var card = card_scene.instantiate()
-		card.position = get_local_mouse_position()
-		card_block.add_child(card)
 
 # esta función es para acusar - se acciona al hacer click en el boton mentiroso o presionar M
 func mentiroso():
